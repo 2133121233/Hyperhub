9e3705462a2783751088054a71b07bbb4fdf1afe
2133121233 <106486318+2133121233@users.noreply.github.com>
2024-02-29 21:05:13 -0300

    Update Menu.lua

--- Menu.lua
+++ Menu.lua
@@ -1,12 +1,12 @@
 local PlaceId = game.PlaceId
 if PlaceId == 6284583030 then -- PSX 
-    loadstring(game:HttpGet("https://gitlab.com/L1ZOT/mango-hub/-/raw/main/Beta-PSX-Mango"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/2133121233/Hyperhub/main/Bloxf.lua"))()
 elseif PlaceId == 7722306047 then -- PSX 
-    loadstring(game:HttpGet("https://gitlab.com/L1ZOT/mango-hub/-/raw/main/Beta-PSX-Mango"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/2133121233/Hyperhub/main/Bloxf.lua"))()
 elseif PlaceId == 2753915549 then -- Blox 
-    loadstring(game:HttpGet("https://gitlab.com/L1ZOT/mango-hub/-/raw/main/Mango-Hub-Blox-Fruits-None-Beta"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/2133121233/Hyperhub/main/Bloxf.lua"))()
 elseif PlaceId == 4442272183 then -- Blox 
-    loadstring(game:HttpGet("https://gitlab.com/L1ZOT/mango-hub/-/raw/main/Mango-Hub-Blox-Fruits-None-Beta"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/2133121233/Hyperhub/main/Bloxf.lua"))()
 elseif PlaceId == 7449423635 then -- Blox 
-    loadstring(game:HttpGet("https://gitlab.com/L1ZOT/mango-hub/-/raw/main/Mango-Hub-Blox-Fruits-None-Beta"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/2133121233/Hyperhub/main/Bloxf.lua"))()
 end
