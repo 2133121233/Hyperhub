c93642bc2944a7def18a4c59e75a94618617ca78
2133121233 <106486318+2133121233@users.noreply.github.com>
2024-03-05 14:45:49 -0300

    Create DISTRIBUTOR.lua

---
+++ DISTRIBUTOR.lua
@@ -0,0 +1,12 @@
+local PlaceId = game.PlaceId
+if PlaceId == 6284583030 then -- PSX 
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/2133121233/Hyperhub/main/Bloxf.lua"))()
+elseif PlaceId == 7722306047 then -- PSX 
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/2133121233/Hyperhub/main/Bloxf.lua"))()
+elseif PlaceId == 2753915549 then -- Blox 
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/2133121233/Hyperhub/main/Bloxf.lua"))()
+elseif PlaceId == 4442272183 then -- Blox 
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/2133121233/Hyperhub/main/Bloxf.lua"))()
+elseif PlaceId == 7449423635 then -- Blox 
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/2133121233/Hyperhub/main/Bloxf.lua"))()
+end
