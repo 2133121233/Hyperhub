70b3f73bb270861cc3c35bd3a2cca801341deb58
2133121233 <106486318+2133121233@users.noreply.github.com>
2024-03-11 18:07:37 -0300

    Update DISTRIBUTOR.lua

--- DISTRIBUTOR.lua
+++ DISTRIBUTOR.lua
@@ -1,7 +1,7 @@
 local PlaceId = game.PlaceId
 if PlaceId == 13772394625 then -- Blade
     loadstring(game:HttpGet("https://raw.githubusercontent.com/2133121233/Test/main/bladeball.lua"))()
-elseif PlaceId == 789768866 then -- PSX 
+elseif PlaceId == 1377238292 then -- Vazio
     loadstring(game:HttpGet("https://raw.githubusercontent.com/2133121233/Hyperhub/main/Bloxf.lua"))()
 elseif PlaceId == 2753915549 then -- Blox 
     loadstring(game:HttpGet("https://raw.githubusercontent.com/2133121233/Hyperhub/main/Bloxf.lua"))()
@@ -9,4 +9,6 @@ elseif PlaceId == 4442272183 then -- Blox
     loadstring(game:HttpGet("https://raw.githubusercontent.com/2133121233/Hyperhub/main/Bloxf.lua"))()
 elseif PlaceId == 7449423635 then -- Blox 
     loadstring(game:HttpGet("https://raw.githubusercontent.com/2133121233/Hyperhub/main/Bloxf.lua"))()
+elseif PlaceId == 5210095481 then -- Omni x
+    loadstring(game:HttpGet('https://pastefy.app/H2xF0S7F/raw'))()
 end
